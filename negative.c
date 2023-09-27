ee66eb8300f4085e97345098f222249513555886
pritam modak <141545535+2006pritam@users.noreply.github.com>
2023-09-27 11:24:35 +0530

    Create negative.c

---
+++ negative.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a==0||a<0)
+{
+printf("the no is negative");
+}
+else
+{
+printf("the number is positive");
+}
+}
