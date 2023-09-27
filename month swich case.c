2f1d1785c41b5f24fb7d736faf30a1d8ebf3706a
pritam modak <141545535+2006pritam@users.noreply.github.com>
2023-09-27 09:53:26 +0530

    Create month swich case.c

---
+++ month swich case.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+main()
+{
+int a;
+scanf("%d",&a);
+switch (a)
+{
+case 1:
+printf("January");
+break;
+case 2:
+printf("February");
+break;
+case 3:
+printf("March");
+break;
+case 4:
+printf("April");
+break;
+case 5:
+printf("may");
+break;
+case 6:
+printf("june");
+break;
+case 7:
+printf("july");
+break;
+case 8:
+printf("August");
+break;
+case 9:
+printf("September");
+break;
+case 10:
+printf("October");
+break;
+case 11:
+printf("November");
+break;
+case 12:
+printf("December");
+break;
+default:
+printf("invalid number");
+}
+}
