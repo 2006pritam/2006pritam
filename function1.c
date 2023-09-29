5d429f76cb04b1acd429ae4274105ca176f71d99
pritam modak <141545535+2006pritam@users.noreply.github.com>
2023-09-29 17:25:51 +0530

    Create function1.c

---
+++ function1.c
@@ -0,0 +1,18 @@
+//function in c//
+#include<stdio.h>
+sum()
+{
+int a,b,s=0;
+printf("enter the number");
+scanf("%d",&a);
+printf("enter the number");
+scanf("%d",&b);
+s=a+b;
+printf("the sum is %d\n",s);
+}
+main()
+{
+sum();
+printf(" return welcome\n");
+sum();
+}
