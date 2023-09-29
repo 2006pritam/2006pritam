c5de4a596ed3e0b88c87be7a2923f2ffbb263993
pritam modak <141545535+2006pritam@users.noreply.github.com>
2023-09-29 18:10:42 +0530

    Create function5.c

---
+++ function5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+int a[5];
+printf("enter the value");
+scanf("%d",&a[0]);
+printf("enter the value");
+scanf("%d",&a[1]);
+printf("enter the value");
+scanf("%d",&a[2]);
+printf("enter the value");
+scanf("%d",&a[3]);
+printf("enter the value");
+scanf("%d",&a[4]);
+printf("%d",a[1]);
+printf("%d",a[4]);
+}
