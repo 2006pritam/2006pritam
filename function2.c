c1b63c2d3cdb378a1966cccd4b5298ba6467fae7
pritam modak <141545535+2006pritam@users.noreply.github.com>
2023-09-29 17:28:06 +0530

    Create function2.c

---
+++ function2.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+sum()
+{
+int a,b,c=0;
+printf("enter the first number");
+scanf("%d",&a);
+printf("enter the first number");
+scanf("%d",&b);
+c=a+b;
+printf("the sum is %d\n",c);
+}
+sub()
+{
+int a,b,c=0;
+printf("enter the first number");
+scanf("%d",&a);
+printf("enter the first number");
+scanf("%d",&b);
+c=a-b;
+printf("the sub is %d\n",c);
+}
+mul()
+{
+int a,b,c=0;
+printf("enter the first number");
+scanf("%d",&a);
+printf("enter the first number");
+scanf("%d",&b);
+c=a*b;
+printf("the mul is %d\n",c);
+}
+div()
+{
+int a,b,c=0;
+printf("enter the first number");
+scanf("%d",&a);
+printf("enter the first number");
+scanf("%d",&b);
+c=a/b;
+printf("the div is %d\n",c);
+}
+main()
+{
+sum();
+sub();
+mul();
+div();
+}
