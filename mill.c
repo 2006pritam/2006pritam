3ff2cd97e73770d2d7588eedabe660e00d03deea
pritam modak <141545535+2006pritam@users.noreply.github.com>
2023-10-04 06:57:43 +0530

    Create mill.c

---
+++ mill.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+int a[10],i;
+printf("enter the value");
+for(i=0;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("the aray is ");
+for(i=0;i<10;i++)
+{
+printf("%d\n",a[i]);
+}
+}
