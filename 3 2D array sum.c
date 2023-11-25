23f5cfcb9ce30e3d1cb54def6729e9d4f551edf1
pritam modak <141545535+2006pritam@users.noreply.github.com>
2023-11-25 09:05:43 +0530

    Create 3 2D array sum.c

---
+++ 3 2D array sum.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    int a[3][3], b[3][3], c[3][3], i, j;
+    printf("Enter elements for matrix A:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("Enter elements for matrix B:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    printf("\nMatrix A:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nMatrix B:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", b[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nSum matrix C:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
